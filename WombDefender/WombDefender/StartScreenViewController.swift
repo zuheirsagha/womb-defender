0267cd3988ce3906f5cb578093cdeee615e5b374
Zuheir Chikh Al Sagha <zuheir.alsagha@gmail.com>
2016-10-08 00:10:24 -0400

    added womb to startpage / attempted gradient

--- WombDefender/WombDefender/StartScreenViewController.swift
+++ WombDefender/WombDefender/StartScreenViewController.swift
@@ -29,13 +29,19 @@ class StartScreenViewController: UIViewController {
         super.viewDidLoad()
         
         UIApplication.shared.statusBarStyle = .lightContent
-        
-//        let startColor = UIColor.red.cgColor
-//        let endColor = UIColor.green.cgColor
-//        let colors = [startColor, endColor]
-//        UIGraphicsBeginImageContext(_backgroundView.frame.size)
-//        let gradient = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: colors as CFArray, locations: [0, 1])
-//        UIGraphicsGetCurrentContext()!.drawRadialGradient(gradient!, startCenter: _backgroundView.center, startRadius: 1, endCenter: _backgroundView.center, endRadius: 1, options: CGGradientDrawingOptions(rawValue: UInt32(0)))
+
+        let colours = [UIColor.red, UIColor.blue] as CFArray
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let gradient = CGGradient(colorsSpace: colorSpace,
+                                  colors: colours ,
+                                  locations: nil)
+        UIGraphicsGetCurrentContext()?.drawRadialGradient(gradient!,
+                           startCenter: view.center,
+                           startRadius: view.bounds.width/5,
+                           endCenter: view.center,
+                           endRadius: view.bounds.width/2,
+                           options: .drawsBeforeStartLocation)
+        _drawWomb()
         
     }
     
@@ -71,4 +77,32 @@ class StartScreenViewController: UIViewController {
      *
      ***********************************************************************************/
     
+    fileprivate func _drawWomb() {
+        
+        let centreCircle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/6), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        let centerLayer = CAShapeLayer()
+        centerLayer.path = centreCircle.cgPath
+        centerLayer.fillColor = UIColor.white.cgColor
+        centerLayer.strokeColor = UIColor.clear.cgColor
+        centerLayer.lineWidth = 3.0
+        view.layer.addSublayer(centerLayer)
+        
+        let layer2Circle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/5), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        let secondLayer = CAShapeLayer()
+        secondLayer.path = layer2Circle.cgPath
+        secondLayer.fillColor = UIColor.white.cgColor
+        secondLayer.strokeColor = UIColor.clear.cgColor
+        secondLayer.lineWidth = 3.0
+        secondLayer.opacity = 0.2
+        view.layer.addSublayer(secondLayer)
+        
+        let layer3Circle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/4.25), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        let thirdLayer = CAShapeLayer()
+        thirdLayer.path = layer3Circle.cgPath
+        thirdLayer.fillColor = UIColor.white.cgColor
+        thirdLayer.strokeColor = UIColor.clear.cgColor
+        thirdLayer.lineWidth = 3.0
+        thirdLayer.opacity = 0.2
+        view.layer.addSublayer(thirdLayer)
+    }
 }
