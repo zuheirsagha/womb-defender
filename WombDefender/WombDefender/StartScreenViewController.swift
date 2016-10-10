4de5accbd694f78913d47c10784263bb4ba630e1
Zuheir Chikh Al Sagha <zuheir.alsagha@gmail.com>
2016-10-09 21:17:51 -0400

    implemented really ugly version of expanding settings menu, nowhere near what I want yet but its kinda there....

--- WombDefender/WombDefender/StartScreenViewController.swift
+++ WombDefender/WombDefender/StartScreenViewController.swift
@@ -21,6 +21,15 @@ class StartScreenViewController: UIViewController {
     
     fileprivate var settingsPressed = false
     
+    fileprivate var secondLayer: CAShapeLayer!
+    fileprivate var layer2Circle: UIBezierPath!
+
+    fileprivate var thirdLayer: CAShapeLayer!
+    fileprivate var layer3Circle: UIBezierPath!
+    
+    fileprivate var centerLayer: CAShapeLayer!
+    fileprivate var centreCircle: UIBezierPath!
+    
     /************************************************************************************
      *
      * LIFECYCLE METHODS
@@ -33,7 +42,27 @@ class StartScreenViewController: UIViewController {
         let gradient = GradientView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
         view.insertSubview(gradient, at: 0)
        
-        _drawWomb()
+        //_drawWomb()
+        
+        layer2Circle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/5), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        secondLayer = CAShapeLayer()
+        secondLayer.path = layer2Circle.cgPath
+        secondLayer.fillColor = UIColor.white.cgColor
+        secondLayer.opacity = 0.2
+        view.layer.addSublayer(secondLayer)
+        
+        layer3Circle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/4.25), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        thirdLayer = CAShapeLayer()
+        thirdLayer.path = layer3Circle.cgPath
+        thirdLayer.fillColor = UIColor.white.cgColor
+        thirdLayer.opacity = 0.2
+        view.layer.addSublayer(thirdLayer)
+        
+        centreCircle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/6), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        centerLayer = CAShapeLayer()
+        centerLayer.path = centreCircle.cgPath
+        centerLayer.fillColor = UIColor.white.cgColor
+        view.layer.addSublayer(centerLayer)
         
     }
     
@@ -59,9 +88,22 @@ class StartScreenViewController: UIViewController {
     }
     @IBAction func onSettingsButtonClicked(_ sender: UIButton) {
         
-        _drawWomb()
+        let scaleFactor = CGFloat(2)
+        var affineTransform = CGAffineTransform(scaleX: scaleFactor, y: scaleFactor)
+        
+        let transformedPathThird = self.layer3Circle.cgPath.copy(using: &affineTransform)
+        self.thirdLayer.path = transformedPathThird
+        
+        let transformedPathSecond = self.layer2Circle.cgPath.copy(using: &affineTransform)
+        self.secondLayer.path = transformedPathSecond
+        
+        let transformedPath = self.centreCircle.cgPath.copy(using: &affineTransform)
+        self.centerLayer.path = transformedPath
+        
+        //_drawWomb()
         
     }
+    
     @IBAction func onShoppingCartButtonClicked(_ sender: UIButton) {
     }
     @IBAction func onStandingsButtonClicked(_ sender: UIButton) {
@@ -75,20 +117,10 @@ class StartScreenViewController: UIViewController {
     
     fileprivate func _drawWomb() {
         
-        let centreCircle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/6), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
-        let centerLayer = CAShapeLayer()
-        centerLayer.path = centreCircle.cgPath
-        centerLayer.fillColor = UIColor.white.cgColor
-        centerLayer.strokeColor = UIColor.clear.cgColor
-        centerLayer.lineWidth = 3.0
-        view.layer.addSublayer(centerLayer)
-        
         let layer2Circle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/5), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
         let secondLayer = CAShapeLayer()
         secondLayer.path = layer2Circle.cgPath
         secondLayer.fillColor = UIColor.white.cgColor
-        secondLayer.strokeColor = UIColor.clear.cgColor
-        secondLayer.lineWidth = 3.0
         secondLayer.opacity = 0.2
         view.layer.addSublayer(secondLayer)
         
@@ -96,13 +128,22 @@ class StartScreenViewController: UIViewController {
         let thirdLayer = CAShapeLayer()
         thirdLayer.path = layer3Circle.cgPath
         thirdLayer.fillColor = UIColor.white.cgColor
-        thirdLayer.strokeColor = UIColor.clear.cgColor
-        thirdLayer.lineWidth = 3.0
         thirdLayer.opacity = 0.2
         view.layer.addSublayer(thirdLayer)
         
+        let centreCircle = UIBezierPath(arcCenter: view.center, radius: CGFloat(view.frame.width/6), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        let centerLayer = CAShapeLayer()
+        centerLayer.path = centreCircle.cgPath
+        centerLayer.fillColor = UIColor.white.cgColor
+        view.layer.addSublayer(centerLayer)
+        
         if settingsPressed {
             // TODO: Animate the womb enlarging -> use this as settings menu
+            let scaleFactor = centerLayer.frame.width*0/centerLayer.frame.height + 2
+            
+            var affineTransform = CGAffineTransform(scaleX: scaleFactor, y: scaleFactor)
+            let transformedPath = centreCircle.cgPath.copy(using: &affineTransform)
+            centerLayer.path = transformedPath
         }
     }
     
---
+++ WombDefender/WombDefender/WombView.swift
@@ -0,0 +1,9 @@
+//
+//  WombView.swift
+//  WombDefender
+//
+//  Created by Zuheir Chikh Al Sagha on 2016-10-09.
+//  Copyright © 2016 Zoko. All rights reserved.
+//
+
+import Foundation
