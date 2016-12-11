c1b4e8ca2503372ee81f9b2f24e3d098d62406d4
Zuheir Chikh Al Sagha <zuheir.alsagha@gmail.com>
2016-12-11 00:24:04 -0500

    fixed localizable string files and changed constraints on tap to start label so that text fit more consistantly among all devices

---
+++ WombDefender/NSLocalizedStringExtension.swift
@@ -0,0 +1,9 @@
+//
+//  NSLocalizedStringExtension.swift
+//  WombDefender
+//
+//  Created by Zuheir Chikh Al Sagha on 2016-12-11.
+//  Copyright © 2016 Zoko. All rights reserved.
+//
+
+import Foundation
